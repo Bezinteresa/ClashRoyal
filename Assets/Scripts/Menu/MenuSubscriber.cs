450c2d88ce1e233acfff1ad52a130ba051272fdc
Ivan <253@bk.ru>
2024-06-21 23:57:44 +0300

    Загрузка карточек

---
+++ Assets/Scripts/Menu/MenuSubscriber.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class MenuSubscriber : MonoBehaviour
+{
+    [SerializeField] private DeckManager _deckManager;
+    [SerializeField] private SelectedDeckUI _selectedDeckUI;
+
+    private void Start()
+    {
+        _deckManager.UpdateSelected += _selectedDeckUI.UpdatecardsList;
+    }
+
+    private void OnDestroy()
+    {
+        _deckManager.UpdateSelected -= _selectedDeckUI.UpdatecardsList;
+    }
+}
---
+++ Assets/Scripts/System/URLLibrary.cs
@@ -0,0 +1,13 @@
+
+public class URLLibrary
+{
+    public const string MAIN = "http://localhost/Projects/LessonDataBase/";
+    public const string AUTHORIZATION = "Authorization/authorization.php";
+    public const string REGISTRATION = "Authorization/registration.php";
+
+
+    public const string GETDECKINFO = "Game/getDeckInfo.php";
+
+   // public static string Authorization { get { return MAIN + AUTHORIZATION;} }
+
+}
